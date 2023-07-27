f6aca3dc1ca34584d5d77713d1a75c2a124035b8
AgengPrayoga <yogaazee@gmail.com>
2023-07-27 11:59:12 +0700

    Update

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+<NavComponent></NavComponent>
+  </div>
+</template>
+
+<script>
+import NavComponent from './NavComponent.vue';
+
+export default{
+  components: {
+NavComponent
+  }
+}
+</script>
